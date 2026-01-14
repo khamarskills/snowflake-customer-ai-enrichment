ef5eb92d080cbc29e098513919751bbc10ff9e36
khamarskills <khamar0118@gmail.com>
2026-01-13 22:16:30 -0500

    Create customer_ai_insights_table SQL script

---
+++ sql/ai/customer_ai_insights_table.sql
@@ -0,0 +1,8 @@
+
+CREATE OR REPLACE TABLE customer_ai_insights_table (
+    customer_id STRING,
+    profile_text STRING,
+    account_summary STRING,
+    recommended_action STRING,
+    generated_ts TIMESTAMP
+);
